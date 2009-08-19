fb0f98428e4ff76772993cc60a535f64b44a5baf
Adam Christian <adam.christian@gmail.com>
2009-08-18 18:14:15 -0700

    First pages commit

--- contrib/wxwindmill/wxwindmill/bin/wxwindmill_bin.py
+++
@@ -1,2 +0,0 @@
-import wxwindmill
-wxwindmill.main()
--- test/framework_tests/files/djangoproject/main/tests.py
+++
@@ -1,8 +0,0 @@
-import os
-from windmill.authoring import djangotest 
-
-from StringIO import StringIO
-
-class TestProjectWindmillTest(djangotest.WindmillDjangoUnitTest): 
-    test_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'windmilltests')
-    browser = 'firefox'
